813880ca728d1bdb7ffad287106b994a591c9995
Raiane Caroline S Teixeira <raianecaroline.st@gmail.com>
2023-12-04 00:19:36 -0300

    Update: Projeto ficando pronto

--- src/main/java/sample/Database/DatabaseHandler.java
+++ src/main/java/sample/Database/DatabaseHandler.java
@@ -2,6 +2,7 @@ package sample.Database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseHandler extends Configs {
@@ -19,6 +20,39 @@ public class DatabaseHandler extends Configs {
 
         return dbConnection;
     }
+
+    //Write
+    public void signUpUser(String firstName, String lastName, String userName,
+                           String password, String location, String gender) {
+
+        String insert = "INSERT INTO"+ Const.USERS_TABLE +"("+ Const.USERS_FIRSTNAME
+                +","+ Const.USERS_LASTNAME +","+ Const.USERS_USERNAME +","+ Const.USERS_PASSWORD
+                +","+ Const.USERS_LOCATION +","+ Const.USERS_Gender +")" + "VALUES(?,?,?,?,?,?)";
+
+
+        try {
+            PreparedStatement preparedStatement = getDbConnection().prepareStatement(insert);
+
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setString(3, userName);
+            preparedStatement.setString(4, password);
+            preparedStatement.setString(5, location);
+            preparedStatement.setString(6, gender);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Read
+    //Update
+    //Delete
+
 
 
 
--- src/main/java/sample/controller/SignupController.java
+++ src/main/java/sample/controller/SignupController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import sample.Database.DatabaseHandler;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -42,6 +43,16 @@ public class SignupController {
 
     @FXML
     void initialize() {
+
+        DatabaseHandler databaseHandler = new DatabaseHandler();
+
+        signUpButton.setOnAction(event -> {
+
+            databaseHandler.signUpUser(signUpFirstName.getText(), signUpLastName.getText(),
+                    signUpUsername.getText(), signUpPassword.getText(),
+                    signUpLocation.getText(), "Female");
+
+        });
 
 
     }
